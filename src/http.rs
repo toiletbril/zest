21dd9e01c79824a07ffc42cecfe5a9dcbba86a9f
toiletbril <s9und@outlook.com>
2023-06-27 00:32:41 +0300

    Refactor http header parsing to support size limit

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,4 @@
-use std::io::{BufRead, BufReader, Write};
+use std::io::{Write, Read, Error, ErrorKind};
 use std::net::TcpStream;
 
 #[derive(Debug)]
@@ -23,7 +23,7 @@ pub struct HttpConnection {
     pub method: HttpMethod,
     pub path: Path,
     pub headers: Headers,
-    pub parameters: Parameters,
+    pub parameters: Option<Parameters>,
 }
 
 impl Write for HttpConnection {
@@ -37,77 +37,85 @@ impl Write for HttpConnection {
 }
 
 use std::collections::HashMap;
+use std::str;
 
 type Headers = HashMap<String, String>;
 type Parameters = HashMap<String, String>;
 type Path = String;
 
-fn parse_http_request(stream: &TcpStream) -> Option<(Parameters, Headers, HttpMethod, Path)> {
-    let mut reader = BufReader::new(stream);
+const MAX_HEADER_SIZE: usize = 4 * 1024;
 
-    let mut request_line = String::new();
-    let _ = reader.read_line(&mut request_line);
-
-    let lines: Vec<String> = reader.lines().filter_map(Result::ok).take_while(|x| x.len() > 2).collect();
-
-    if lines.is_empty() {
-        return None;
-    }
-
-    let request_line = &request_line[..request_line.len() - 2];
-
-    let parts: Vec<&str> = request_line.split_whitespace().collect();
-    if parts.len() < 3 {
-        return None;
-    }
-
-    let method = parts[0].to_string();
-    let path = parts[1].to_string();
-
-    let mut parameters = HashMap::new();
+fn parse_http_headers(stream: &mut TcpStream) -> Result<(Headers, Option<Parameters>, HttpMethod, Path), Error> {
     let mut headers = HashMap::new();
+    let mut parameters = None;
 
-    for line in lines {
-        if line.is_empty() {
-            continue;
-        }
+    let mut method = HttpMethod::UNKN;
+    let mut path = String::new();
 
-        let parts: Vec<&str> = line.splitn(2, ':').map(|part| part.trim()).collect();
-        if parts.len() != 2 {
-            continue;
-        }
+    let mut total_bytes_read = 0;
+    let mut current_line = String::new();
+    let mut prev_character: Option<char> = None;
+    let mut buffer = [0; 1];
 
-        let key = parts[0].to_string();
-        let value = parts[1].to_string();
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(_) => {
+                let character = buffer[0] as char;
+                total_bytes_read += 1;
 
-        if key.eq_ignore_ascii_case("Host") {
-            if let Some(index) = value.find(':') {
-                let host = value[..index].to_string();
-                let port = value[(index + 1)..].to_string();
-                headers.insert("Host".to_string(), host);
-                headers.insert("Port".to_string(), port);
-            } else {
-                headers.insert("Host".to_string(), value);
+                if character == '\n' {
+                    if path.is_empty() {
+                        let parsed = parse_request_line(&current_line);
+                        if let Ok((method_parsed, path_parsed, parameters_parsed)) = parsed {
+                            method = method_parsed;
+                            path = path_parsed;
+                            parameters = parameters_parsed;
+                        } else {
+                            return Err(parsed.unwrap_err());
+                        }
+                    }
+
+                    if prev_character == Some('\r') && current_line.is_empty() {
+                        return Ok((headers, parameters, method, path));
+                    }
+                    else {
+                        let _ = parse_header_line(&current_line, &mut headers);
+                    }
+
+                    current_line.clear();
+                } else if character != '\r' {
+                    current_line.push(character);
+                }
+
+                prev_character = Some(character);
             }
-        } else {
-            headers.insert(key, value);
+            Err(err) => return Err(err), // error reading from the stream
+        }
+
+        // header size exceeded 8KB
+        if total_bytes_read > MAX_HEADER_SIZE {
+            let message = format!("Header size exceeded {} bytes.", MAX_HEADER_SIZE);
+            let err = std::io::Error::new(ErrorKind::OutOfMemory, message);
+            return Err(err);
         }
     }
 
-    if let Some(index) = path.find('?') {
-        let parameter_string = &path[(index + 1)..];
-        let parameter_vec: Vec<&str> = parameter_string.split('&').collect();
+    let message = format!("Malformed headers.");
+    let err = std::io::Error::new(ErrorKind::InvalidData, message);
+    return Err(err);
+}
 
-        for parameter in parameter_vec {
-            if let Some(index) = parameter.find('=') {
-                let key = &parameter[..index];
-                let value = &parameter[(index + 1)..];
-                parameters.insert(key.to_string(), value.to_string());
-            }
-        }
+fn parse_request_line(line: &str) -> Result<(HttpMethod, Path, Option<Parameters>), Error> {
+    let parts: Vec<&str> = line.split_whitespace().collect();
+
+    if parts.len() < 2 {
+        let message = "Invalid request line.";
+        let err = Error::new(ErrorKind::InvalidInput, message);
+        return Err(err);
     }
 
-    let method = match method.as_str() {
+    let method = match parts[0] {
         "GET" => HttpMethod::GET,
         "POST" => HttpMethod::POST,
         "PUT" => HttpMethod::PUT,
@@ -116,9 +124,68 @@ fn parse_http_request(stream: &TcpStream) -> Option<(Parameters, Headers, HttpMe
         _ => HttpMethod::UNKN,
     };
 
-    let path = path.split('?').next().unwrap_or("/").to_string();
+    let raw_path = parts[1].to_owned();
+    let mut path_split = raw_path.split('?');
 
-    Some((parameters, headers, method, path))
+    let path = if let Some(path) = path_split.next() {
+        path.to_owned()
+    } else {
+        "/".to_owned()
+    };
+
+    let parameters = if let Some(raw_parameters) = path_split.next() {
+        let mut parameters = HashMap::new();
+        let split_parameters = raw_parameters.split(',');
+
+        for param in split_parameters {
+            let mut kv = param.split('=');
+            let (k, v);
+
+            k = if let Some(key) = kv.next() {
+                key
+            } else {
+                let message = "Invalid parameter key.";
+                let err = Error::new(ErrorKind::InvalidInput, message);
+                return Err(err);
+            };
+
+            v = if let Some(value) = kv.next() {
+                value
+            } else {
+                ""
+            };
+
+            parameters.insert(k.to_string(), v.to_string());
+        }
+
+        Some(parameters)
+    } else {
+        None
+    };
+
+    Ok((method, path, parameters))
+}
+
+fn parse_header_line(line: &str, headers: &mut HashMap<String, String>) -> Result<(), Error> {
+    let mut split = line.split(": ");
+
+    let key = if let Some(key) = split.next() {
+        key
+    } else {
+        let message = "Invalid header line.";
+        let err = Error::new(ErrorKind::InvalidInput, message);
+        return Err(err);
+    };
+
+    let value = if let Some(value) = split.next() {
+        value
+    } else {
+        ""
+    };
+
+    headers.insert(key.to_string(), value.to_string());
+
+    Ok(())
 }
 
 impl Drop for HttpConnection {
@@ -127,24 +194,21 @@ impl Drop for HttpConnection {
     }
 }
 
-impl From<TcpStream> for HttpConnection {
-    fn from(stream: TcpStream) -> Self {
-        if let Some((parameters, headers, method, path)) = parse_http_request(&stream) {
-            HttpConnection {
-                stream,
-                method,
-                path,
-                headers,
-                parameters,
-            }
+impl HttpConnection {
+    pub fn new(mut stream: TcpStream) -> Result<Self, Error> {
+        let result = parse_http_headers(&mut stream);
+        if let Ok((headers, parameters, method, path)) = result {
+            Ok(
+                HttpConnection {
+                    stream,
+                    method,
+                    path,
+                    headers,
+                    parameters,
+                }
+            )
         } else {
-            HttpConnection {
-                stream,
-                method: HttpMethod::UNKN,
-                path: Path::new(),
-                headers: Headers::new(),
-                parameters: Parameters::new(),
-            }
+            Err(result.unwrap_err())
         }
     }
 }
