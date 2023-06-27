97f54d3ef0ecd64f345477fcb52f2556d261511d
toiletbril <s9und@outlook.com>
2023-06-28 01:51:07 +0300

    Move custom types to common

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,6 +1,9 @@
 pub mod logger;
 pub mod threads;
 
-use std::sync::{Arc, Mutex};
+use std::{sync::{Arc, Mutex}, collections::HashMap};
 
 pub type Am<T> = Arc<Mutex<T>>;
+pub type FilePath = String;
+pub type FileName = String;
+pub type MusicIndex = HashMap<FileName, FilePath>;
