8a3eff4223cabb2bc8f48c3b87b174f75bf6646a
toiletbril <s9und@outlook.com>
2023-07-03 17:24:47 +0300

    I DESPISE JAVASCRIPT AND WEB !!!!!!!!!!!!

--- player/README.md
+++ player/README.md
@@ -1,3 +1,16 @@
-# Example frontend
+# Example player
 
-This example assumes that Zest is running on `http://localhost:6969` and your browser supports decoding of `audio/mpeg`.
+This example assumes that Zest is running on `http://localhost:6969` and your browser supports decoding of `audio/mpeg` with `MediaSource`.
+It also relies on `SourceBuffer` to store audio in memory, which means that max size for one track is ~10mb.
+
+# Quick start
+
+Start Zest on default port:
+```console
+$ cargo run --release -- serve ...
+```
+
+Start a python web-server to serve this folder:
+```console
+$ python3 -m http.server <port>
+```
--- player/main.js
+++ player/main.js
@@ -5,45 +5,24 @@ let totalDuration = 0;
 
 const audioPlayer = document.getElementById("audio-player");
 
-let mediaSource;
-let sourceBuffer;
-
-function fetchAndPlayMusic(trackName, chunkIndex = 0) {
-    const url = endpoint + `/get?name=${encodeURIComponent(trackName)}&chunk=${chunkIndex}`;
-
-    mediaSource = new MediaSource();
+function fetchMusic(trackName) {
+    let mediaSource = new MediaSource();
     audioPlayer.src = URL.createObjectURL(mediaSource);
 
     document.getElementById("track-name").textContent = trackName;
 
     mediaSource.addEventListener("sourceopen", () => {
-        sourceBuffer = mediaSource.addSourceBuffer("audio/mpeg");
-        fetchAndAppendChunk(sourceBuffer, mediaSource, url);
+        const url = endpoint + `/get?name=${encodeURIComponent(trackName)}&chunk=0`;
+        let sourceBuffer = mediaSource.addSourceBuffer("audio/mpeg");
+        appendChunks(sourceBuffer, mediaSource, url);
     });
 
     mediaSource.addEventListener("error", error => {
         console.error("Error opening media source:", error);
     });
-
-    mediaSource.addEventListener("sourceended", () => {
-        audioPlayer.duration = totalDuration;
-    });
 }
 
-function resetPlayback() {
-    totalDuration = 0;
-    if (mediaSource) {
-        if (sourceBuffer) {
-            sourceBuffer.removeEventListener("updateend", handleUpdateEnd);
-            mediaSource.removeSourceBuffer(sourceBuffer);
-        }
-        if (mediaSource.readyState === "open") {
-            mediaSource.endOfStream();
-        }
-    }
-}
-
-function fetchAndAppendChunk(sourceBuffer, mediaSource, url) {
+function appendChunks(sourceBuffer, mediaSource, url) {
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -52,16 +31,16 @@ function fetchAndAppendChunk(sourceBuffer, mediaSource, url) {
             return response.arrayBuffer();
         })
         .then(arrayBuffer => {
-            if (sourceBuffer.updating) {
-                const appendInterval = setInterval(() => {
-                    if (!sourceBuffer.updating) {
-                        clearInterval(appendInterval);
-                        appendNextChunk(sourceBuffer, mediaSource, url, arrayBuffer);
-                    }
-                }, 100);
-            } else {
-                appendNextChunk(sourceBuffer, mediaSource, url, arrayBuffer);
-            }
+            const appendInterval = setInterval(() => {
+                if (!sourceBuffer.updating) {
+                    clearInterval(appendInterval);
+
+                    mediaSource.duration = totalDuration;
+                    audioPlayer.duration = mediaSource.duration;
+
+                    appendNextChunk(sourceBuffer, mediaSource, url, arrayBuffer);
+                }
+            }, 100);
         })
         .catch(error => console.error("Error fetching music chunk:", error));
 }
@@ -70,39 +49,36 @@ const CHUNK_SIZE = 1024 * 128; // 128 kb
 
 function appendNextChunk(sourceBuffer, mediaSource, url, arrayBuffer) {
     sourceBuffer.appendBuffer(arrayBuffer);
+
     const isLastChunk = arrayBuffer.byteLength < CHUNK_SIZE;
 
     if (!isLastChunk) {
         const nextChunkIndex = parseInt(new URL(url).searchParams.get("chunk")) + 1;
         const nextUrl = url.replace(/chunk=\d+/, `chunk=${nextChunkIndex}`);
-        fetchAndAppendChunk(sourceBuffer, mediaSource, nextUrl);
+        appendChunks(sourceBuffer, mediaSource, nextUrl);
     }
 
-    updatingBuffers++;
-    sourceBuffer.addEventListener("updateend", handleUpdateEnd);
+    extendDuration(arrayBuffer);
 }
 
-function handleUpdateEnd() {
-    updatingBuffers--;
-    if (updatingBuffers === 0) {
-        mediaSource.duration = totalDuration;
-        audioPlayer.duration = mediaSource.duration;
-    }
-}
-
-function getChunkDuration(arrayBuffer) {
+function extendDuration(arrayBuffer) {
     const audioContext = new AudioContext();
-    return audioContext.decodeAudioData(arrayBuffer)
-        .then(decodedData => decodedData.duration)
-        .catch(error => {
-            console.error("Error decoding audio data:", error);
-            return 0;
+    audioContext.decodeAudioData(arrayBuffer)
+        .then(decodedData => {
+            totalDuration += decodedData.duration;
         });
+}
+
+function resetPlayback() {
+    totalDuration = 0;
+    updatingBuffers = 0;
 }
 
 function playTrack(trackName) {
     resetPlayback();
-    fetchAndPlayMusic(trackName);
+    fetchMusic(trackName);
+    audioPlayer.currentTime = 0;
+    audioPlayer.play();
 }
 
 let allTracks = [];
@@ -130,6 +106,10 @@ function searchTracks(event) {
 }
 
 function updateMusicList(tracks) {
+    if (tracks.length > 100) {
+        tracks = tracks.slice(0, 100);
+    }
+
     const tracksDiv = document.getElementById("tracks");
     tracksDiv.innerHTML = "";
 
@@ -156,7 +136,8 @@ window.onload = () => {
     searchInput.addEventListener("keyup", searchTracks);
 
     if (!MediaSource.isTypeSupported("audio/mpeg")) {
-        alert("Your browser does not support decoding of audio/mpeg, and playing music will not work.\n\n" +
+        alert(
+            "Your browser does not support decoding of audio/mpeg, and playing music will not work.\n\n" +
             "To fix this, please install a compatible decoder, such as ffmpeg.");
     }
 };
